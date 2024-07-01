df518e6896b58d346301ceb01e6d2b253a01e7e7
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-07-01 16:42:11 -0300

    algafood-auth projeto

---
+++ src/main/java/com/algaworks/core/JpaUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.algaworks.core;
+
+import com.algaworks.domain.Usuario;
+import com.algaworks.domain.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com e-mail informado."));
+
+        return new AuthUser(usuario);
+    }
+}
